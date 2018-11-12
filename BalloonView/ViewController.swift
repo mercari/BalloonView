8ddd55c4f6b27026e8986233db1d1d8ccc463911
Teddy Ku <teddypku@gmail.com>
2018-11-12 16:36:28 +0900

    Initial Commit

---
+++ BalloonView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BalloonView
+//
+//  Created by teddy on 11/12/18.
+//  Copyright © 2018 merpay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BalloonView
 Makes view with tooltip. Useful for onboarding tutorials
+
+## Contribution
+
+Please read the CLA below carefully before submitting your contribution.
+
+https://www.mercari.com/cla/
+
+## License
+
+Copyright 2018 Mercari, Inc.
+
+Licensed under the MIT License.
